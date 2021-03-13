b53b3997be850206e9edee1ed37a1aba202ee7ae
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2021-03-13 04:41:01 -0600

    updated the reviewservice

--- src/main/java/com/learnreactiveprogramming/service/ReviewService.java
+++ src/main/java/com/learnreactiveprogramming/service/ReviewService.java
@@ -7,17 +7,17 @@ import java.util.List;
 
 public class ReviewService {
 
-    public  List<Review> retrieveReviews(long MovieId){
+    public  List<Review> retrieveReviews(long movieInfoId){
 
-        var reviewsList = List.of(new Review(MovieId, "Awesome Movie", 8.9),
-                new Review(MovieId, "Excellent Movie", 9.0));
-        return reviewsList;
+        return List.of(new Review(1L, movieInfoId, "Awesome Movie", 8.9),
+                new Review(2L, movieInfoId, "Excellent Movie", 9.0));
     }
 
-    public Flux<Review> retrieveReviewsFlux(long MovieId){
+    public Flux<Review> retrieveReviewsFlux(long movieInfoId){
 
-        var reviewsList = List.of(new Review(MovieId, "Awesome Movie", 8.9),
-                new Review(MovieId, "Excellent Movie", 9.0));
+        var reviewsList = List.of(new Review(1L,movieInfoId, "Awesome Movie", 8.9),
+                new Review(2L, movieInfoId, "Excellent Movie", 9.0));
         return Flux.fromIterable(reviewsList);
     }
+
 }
