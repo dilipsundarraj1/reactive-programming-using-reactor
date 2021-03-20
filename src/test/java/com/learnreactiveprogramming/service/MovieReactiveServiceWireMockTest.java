2aafc552336e50c3101c54edec8feca880568c8d
Dilip Sundarraj <you@example.com>
2021-03-20 12:40:47 -0500

    wiremock setup in progress

--- src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceWireMockTest.java
+++ src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceWireMockTest.java
@@ -1,13 +1,29 @@
 package com.learnreactiveprogramming.service;
 
+import com.github.jenspiegsa.wiremockextension.InjectServer;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MovieReactiveServiceRestClientTest {
+@ExtendWith(WireMockExtension.class)
+public class MovieReactiveServiceWireMockTest {
+
+    @InjectServer
+    WireMockServer wireMockServer;
 
     WebClient webClient = WebClient.builder()
             .baseUrl("http://localhost:8080/movies")
@@ -17,27 +33,22 @@ public class MovieReactiveServiceRestClientTest {
     RevenueService revenueService = new RevenueService();
     MovieReactiveService movieReactiveService = new MovieReactiveService(mis, rs,revenueService);
 
+
     @Test
-    @Disabled
     void getAllMovies_RestClient() {
         //given
+        stubFor(get(urlPathEqualTo("/movies/v1/movie_infos"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("all-movies.json")));
 
-        //when
-        var moviesFlux = movieReactiveService.getAllMovies_RestClient();
-
-        //then
-        StepVerifier.create(moviesFlux)
-                .assertNext(movie -> {
-                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
-                    assertEquals("Nolan is the real superhero", movie.getReviewList().get(0).getComment());
-                })
-                .expectNextCount(6)
-                .verifyComplete();
-    }
-
-    @Test
-    void getAllMovies_RestClient_Wiremock() {
-        //given
+        stubFor(get(urlPathEqualTo("/movies/v1/reviews"))
+                //.withQueryParam("movieInfoId", )
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("reviews.json")));
 
         //when
         var moviesFlux = movieReactiveService.getAllMovies_RestClient();
