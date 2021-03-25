b1463b97b73abc085bb086cbe732d3a280d8371d
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2021-03-25 04:56:30 -0500

    added the flux and mono Schedulers service

---
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -0,0 +1,17 @@
+package com.learnreactiveprogramming.service;
+
+import java.util.List;
+
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
+
+public class FluxAndMonoSchedulersService {
+
+    static List<String> namesList = List.of("alex", "ben", "chloe");
+    static List<String> namesList1 = List.of("adam", "jill", "jack");
+
+    private String upperCase(String name) {
+        delay(1000);
+        return name.toUpperCase();
+    }
+
+}
